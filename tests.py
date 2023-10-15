af6624af1371ef0215e0871ee2e36ca283f33059
Mylo Bishop <mylo.bishops@gmail.com>
2023-10-15 15:41:06 +0100

    Added tests.py, test_start_bitboards

---
+++ tests.py
@@ -0,0 +1,22 @@
+from board import Connect4Board
+
+
+def test_start_bitboards():
+    test = Connect4Board()
+    assert test.player0_bitboard == 0b0
+    assert test.player1_bitboard == 0b0
+    assert test.turn == 0b0
+    assert test.bottom_mask == 0b1000000100000010000001000000100000010000001000000
+    assert test.top_of_columns == 0b0000001000000100000010000001000000100000010000001
+
+    test = Connect4Board(6, 7)
+    assert test.bottom_mask == 0b100000001000000010000000100000001000000010000000
+    assert test.top_of_columns == 0b000000010000000100000001000000010000000100000001
+
+    test = Connect4Board(4, 4)
+    assert test.bottom_mask == 0b10000100001000010000
+    assert test.top_of_columns == 0b00001000010000100001
+
+    test = Connect4Board(10, 10)
+    assert test.bottom_mask == 0x2004008010020040080100200400
+    assert test.bottom_mask == 0x8010020040080100200400801
