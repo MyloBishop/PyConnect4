5693f35c7beb47a38b8e71177bea7609fe2f15e9
Mylo Bishop <mylo.bishops@gmail.com>
2023-10-19 18:32:36 +0100

    more ordering, ab pruning

--- ai/src/negamax.py
+++ ai/src/negamax.py
@@ -1,33 +1,46 @@
+from math import inf
+
 from board import Board
 
 
-def negamax(node):
-    node.display(clear=False)
+def negamax(node, depth: int = inf, alpha: int = -inf, beta: int = inf):
+    if depth == 0 or node.is_terminal:
+        negation = -1 if node.turn else 1
+        return negation * score(node), None
 
-    if node.is_terminal:
-        return score(node)
-
-    value = float("-inf")
+    value = -inf
     moves = node.get_legal_moves()
+    moves = order_moves(moves)
 
     for move in moves:
         node.make_move(move)
-        value = max(value, -negamax(node))
+        new_value, _ = negamax(node, depth - 1, -beta, -alpha)
+        new_value = -new_value
+        if new_value > value:
+            value = new_value
+            best_move = move
+        alpha = max(alpha, value)
         node.undo_move(move)
-    return value
+        if alpha >= beta:
+            break
+    return value, best_move
+
+
+def order_moves(moves):
+    # Center column first, then head outwards
+    center = 3
+    moves.sort(key=lambda move: abs(move - center))
+    return moves
 
 
 def score(node):
     if node.is_win(0):
-        print("Player 0 wins")
         return 22 - node.player0_bitboard.bit_count()
     if node.is_win(1):
-        print("Player 1 wins")
         return node.player1_bitboard.bit_count() - 22
-    print("Draw.")
     return 0  # Draw
 
 
-root = Board(position="144324431445513573673777361765615215226")
+root = Board(position="11233566724171221527127745")
 root.display()
 print(negamax(root))
